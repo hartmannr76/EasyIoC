d923ee2c17e0ef13cdf1e00632cc975eec5c4327
hartmannr76 <hartmannr76@gmail.com>
2016-12-22 16:42:45 -0500

    Adds some testing infra as well as testing in CI

---
+++ EasyIoC.Tests/AttributeBasedFinderTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using EasyIoC.Attributes;
+using Moq;
+using NUnit.Framework;
+
+namespace EasyIoC.Tests
+{
+    [TestFixture]
+    public class AttributeBasedFinderTests
+    {
+        private AttributeBasedFinder _service;
+        private List<Assembly> _mockAssemblies;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new AttributeBasedFinder();
+            _mockAssemblies = null;
+        }
+
+        [Test]
+        public void FindRegisteredClasses_GivenClassWithAttribute_ReturnsType()
+        {
+            GivenAssembliesHasType(typeof(TestableWithAttributeClass));
+
+            var result = _service.FindRegisteredClasses(_mockAssemblies);
+
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(typeof(TestableWithAttributeClass), result.First());
+        }
+
+        [Test]
+        public void FindRegisteredClasses_GivenClassWithNoAttribute_ReturnsNoType()
+        {
+            GivenAssembliesHasType(typeof(TestableWithoutAttributeClass));
+
+            var result = _service.FindRegisteredClasses(_mockAssemblies);
+
+            Assert.AreEqual(0, result.Count());
+        }
+
+        private void GivenAssembliesHasType(Type type)
+        {
+            var assemblyList = _mockAssemblies ?? new List<Assembly>();
+
+            var mockAssembly = new Mock<Assembly>();
+            mockAssembly.SetupGet(x => x.DefinedTypes)
+                .Returns(() => new[] {type.GetTypeInfo()});
+
+            assemblyList.Add(mockAssembly.Object);
+
+            _mockAssemblies = assemblyList;
+        }
+    }
+
+    [AutoRegister]
+    public class TestableWithAttributeClass {}
+
+    public class TestableWithoutAttributeClass {}
+}
---
+++ EasyIoC.Tests/ServiceRegistrarTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace EasyIoC.Tests
+{
+    [TestFixture]
+    public class ServiceRegistrarTests
+    {
+        private ServiceRegistrar _service;
+        private Mock<IServiceContainer> _mockCollection;
+
+        [SetUp]
+        public void Setup()
+        {
+            _service = new ServiceRegistrar();
+            _mockCollection = new Mock<IServiceContainer>();
+        }
+
+        [Test]
+        public void RegisterTypeForLifetime_GivenSingleton_RegistersCorrectly()
+        {
+            _service.RegisterTypeForLifetime(_mockCollection.Object, typeof(TestableClass), ServiceLifetime.Singleton);
+
+            _mockCollection.Verify(x => x.AddSingleton(typeof(ITestableClass), typeof(TestableClass)), Times.Once);
+        }
+
+        [Test]
+        public void RegisterTypeForLifetime_GivenTransient_RegistersCorrectly()
+        {
+            _service.RegisterTypeForLifetime(_mockCollection.Object, typeof(TestableClass), ServiceLifetime.Transient);
+
+            _mockCollection.Verify(x => x.AddTransient(typeof(ITestableClass), typeof(TestableClass)), Times.Once);
+        }
+
+        [Test]
+        public void RegisterTypeForLifetime_GivenRequestScoped_RegistersCorrectly()
+        {
+            _service.RegisterTypeForLifetime(_mockCollection.Object, typeof(TestableClass), ServiceLifetime.PerRequest);
+
+            _mockCollection.Verify(x => x.AddRequestScoped(typeof(ITestableClass), typeof(TestableClass)), Times.Once);
+        }
+    }
+
+    interface ITestableClass {}
+
+    public class TestableClass : ITestableClass {}
+}
