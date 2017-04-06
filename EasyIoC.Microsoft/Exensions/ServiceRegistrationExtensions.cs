1182fd752912188a1846c5bcad0ee81d9a5819d3
hartmannr76 <hartmannr76@gmail.com>
2017-04-06 04:47:42 -0400

    Adding some protections

--- EasyIoC.Microsoft/Exensions/ServiceRegistrationExtensions.cs
+++ EasyIoC.Microsoft/Exensions/ServiceRegistrationExtensions.cs
@@ -3,7 +3,12 @@ using EasyIoC.Finders;
 
 namespace EasyIoC.Microsoft.Exensions {
     public static class ServiceRegistrationExtensions {
-        public static void AutoRegisterServices(this IServiceCollection collection, IClassFinder classFinder) {
+		/// <summary>
+		/// Registers the first discovered occurrence of 
+		/// </summary>
+		/// <param name="collection">Collection.</param>
+		/// <param name="classFinder">Class finder.</param>
+        public static void RegisterDependencies(this IServiceCollection collection, IClassFinder classFinder) {
             var assemblyFinder = new AssemblyFinder();
             var assemblies = assemblyFinder.FindAssemblies(null);
 
@@ -14,5 +19,26 @@ namespace EasyIoC.Microsoft.Exensions {
                 classFinder.RegisterClass(item, serviceCollection);
             }
         }
+
+		/// <summary>
+		/// Registers the first discovered occurrence of 
+		/// </summary>
+		/// <param name="collection">Collection.</param>
+		/// <param name="classFinder">Class finder.</param>
+		public static void RegisterDependencies(
+			this IServiceCollection collection,
+			IClassFinder classFinder)
+		{
+			var assemblyFinder = new AssemblyFinder();
+			var assemblies = assemblyFinder.FindAssemblies(null);
+
+			var classesToRegister = classFinder.FindRegisteredClasses(assemblies);
+			var serviceCollection = new ServiceContainer(collection);
+
+			foreach (var item in classesToRegister)
+			{
+				classFinder.RegisterClass(item, serviceCollection);
+			}
+		}
     }
 }
