83bf463d8915549f1bddc2a40cd1a2875e3f2851
hartmannr76 <hartmannr76@gmail.com>
2017-04-06 05:00:07 -0400

    Build from csproj now

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 set -e
 
 # Make sure all solutions build properly
-for proj in `ls */project.json`; do
+for proj in `ls */*.csproj`; do
     dotnet build "$proj";
 done
 
 # Run any unit tests we have for these solutions
-for proj in `ls *Tests*/project.json`; do
+for proj in `ls *Tests*/*.csproj`; do
     dotnet test "$proj";
 done
