c4d57241518455abaaf2384c795ba070fb57c0d1
hartmannr76 <hartmannr76@gmail.com>
2017-04-07 18:19:54 -0400

    Allows a user to pass in a func to determine if the env should be used

--- EasyIoC.Microsoft/Exensions/ServiceRegistrationExtensions.cs
+++ EasyIoC.Microsoft/Exensions/ServiceRegistrationExtensions.cs
@@ -1,14 +1,20 @@
 using Microsoft.Extensions.DependencyInjection;
 using EasyIoC.Finders;
+using System;
+using System.Collections.Generic;
 
 namespace EasyIoC.Microsoft.Exensions {
     public static class ServiceRegistrationExtensions {
 		/// <summary>
 		/// Registers the first discovered occurrence of 
 		/// </summary>
-		/// <param name="collection">Collection.</param>
-		/// <param name="classFinder">Class finder.</param>
-        public static void RegisterDependencies(this IServiceCollection collection, IClassFinder classFinder, string Environment = null) {
+		/// <param name="collection">Service collection</param>
+		/// <param name="classFinder">EasyIoC finder</param>
+        /// <param name="Environment">Environment to register instances for</param>
+        public static void RegisterDependencies(
+            this IServiceCollection collection,
+            IClassFinder classFinder,
+            string Environment = null) {
             var assemblyFinder = new AssemblyFinder();
             var assemblies = assemblyFinder.FindAssemblies(null);
 
@@ -16,7 +22,30 @@ namespace EasyIoC.Microsoft.Exensions {
             var serviceCollection = new ServiceContainer(collection);
 
             foreach(var item in classesToRegister) {
-                classFinder.RegisterClass(item, serviceCollection, Environment);
+                classFinder.RegisterClass(item.Type, serviceCollection, Environment);
+            }
+        }
+
+        public static void RegisterDependencies(
+            this IServiceCollection collection,
+            IClassFinder classFinder,
+            Func<string, bool> canRegister) {
+            var assemblyFinder = new AssemblyFinder();
+            var assemblies = assemblyFinder.FindAssemblies(null);
+            Dictionary<string, bool> registeredResponses = new Dictionary<string, bool>();
+
+            var classesToRegister = classFinder.FindRegisteredClasses(assemblies);
+            var serviceCollection = new ServiceContainer(collection);
+
+            foreach (var item in classesToRegister) {
+                if (!registeredResponses.ContainsKey(item.Environment)) {
+                    var envCanRegister = canRegister(item.Environment);
+                    registeredResponses.Add(item.Environment, envCanRegister);
+                }
+
+                if (registeredResponses[item.Environment]) {
+                    classFinder.RegisterClass(item.Type, serviceCollection, null);
+                }
             }
         }
     }
